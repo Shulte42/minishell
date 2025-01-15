c44657564d7e710126814867dc75defa4cde58d5
Brunno Da Silva Shulte <bruda-si@c2r8s6.42lisboa.com>
2025-01-15 17:25:49 +0000

    retorno get value pra nada: ''

--- src/type_tokens.c
+++ src/type_tokens.c
@@ -6,7 +6,7 @@
 /*   By: bruda-si <bruda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 13:11:29 by bruda-si          #+#    #+#             */
-/*   Updated: 2025/01/15 13:27:38 by bruda-si         ###   ########.fr       */
+/*   Updated: 2025/01/15 15:11:35 by bruda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ void    ft_type_tokens(t_tokens *tokens)
 	while (node->next)
 	{
 		if(ft_strcmp(node->token, "echo") == 0)
-		
+			return;
 	}
 }
