a27a32fd8bac713e2c80020aed6ad2001e1fde46
shulte42 <shulte42@gmail.com>
2025-01-29 16:11:22 +0000

    a!

--- src/type_tokens.c
+++ src/type_tokens.c
@@ -6,22 +6,33 @@
 /*   By: shulte <shulte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 13:11:29 by bruda-si          #+#    #+#             */
-/*   Updated: 2025/01/21 10:13:08 by shulte           ###   ########.fr       */
+/*   Updated: 2025/01/29 15:55:32 by shulte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/libs.h"
 
-void    ft_set_token_type(t_tokens *tokens)
+void	ft_set_token_type(t_tokens *lst)
 {
-	t_tokens	*node;
+	t_tokens	*current;
 
-	node = tokens;
-	while (node->next)
+	current = lst;
+	while (current)
 	{
-		// if (ft_builtins(&node) == 0)
-		// {
-		// 	exec_builtins(node);
-		// }
+		if (ft_strcmp(current->content, ">") == 0)
+			current->type = REDIR_OUT;
+		else if (ft_strcmp(current->content, ">>") == 0)
+			current->type = APPEND_OUT;
+		else if (ft_strcmp(current->content, "<") == 0)
+			current->type = REDIR_IN;
+		else if (ft_strcmp(current->content, "<<") == 0)
+			current->type = HEREDOC;
+		else if (ft_strcmp(current->content, "|") == 0)
+			current->type = PIPE;
+		else if (!current->prev || (current->prev->type >= REDIR_OUT))
+			current->type = CMD;
+		else
+			current->type = ARG;
+		current = current->next;
 	}
 }
