ec51d32002672db7b54419d43d825ccaab6cb94c
Shulte42 <146744333+Shulte42@users.noreply.github.com>
2025-01-29 13:47:51 +0000

    Add files via upload

---
+++ src/input_analizes.c
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_analizes.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luiz-dos <luiz-dos@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/26 15:18:15 by shulte            #+#    #+#             */
+/*   Updated: 2025/01/14 14:29:07 by luiz-dos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/libs.h"
+
+bool	ft_check_piperedir(char	c)
+{
+	if (c == '|' || c == '<' || c == '>')
+		return (true);
+	return (false);
+}
+
+int	ft_c_piperedir(char	*str)
+{
+	int	count;
+	int	i;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (ft_check_piperedir(str[i]))
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+// static void add_space_around_piperedir(char	*input, char *input_spaced, int *i, int *z)
+// {
+// 	if (ft_inside_quotes(input, *i) == 0 && ft_check_piperedir(input[*i]))
+	// {
+	// 	input_spaced[(*z)++] = ' ';
+	// 	input_spaced[*z] = input[*i];
+	// 	if (input[*i + 1] == input[*i])
+	// 	{
+	// 		input_spaced[++(*z)] = input[++(*i)];
+	// 		input_spaced[++(*z)] = ' ';
+	// 	}
+	// 	else if (input[*i + 1] != input[(*i)] && input[*i + 1])
+	// 		input_spaced[++(*i)] = ' ';
+	// }
+	// else
+	// 	input_spaced[*z] = input[*i];
+// }
+
+void    ft_space_piperedir(t_shell  *shell)
+{
+	char    *input_spaced;
+	int     z;
+	int		i;
+	
+	i = 0;
+	z = 0;
+	input_spaced = ft_calloc(ft_strlen(shell->input) + (ft_c_piperedir(shell->input) * 2) + 1, sizeof(char));
+	while (shell->input[i])
+	{
+		if (ft_inside_quotes(shell->input, i) == 0 && ft_check_piperedir(shell->input[i]))
+			{
+				input_spaced[z++] = ' ';
+				input_spaced[z] = shell->input[i];
+				if (shell->input[i + 1] == shell->input[i])
+				{
+					input_spaced[++z] = shell->input[++i];
+					input_spaced[++z] = ' ';
+ 				}
+				else if (shell->input[i + 1] != shell->input[i] && shell->input[i + 1])
+					input_spaced[++z] = ' ';
+			}
+		else
+			input_spaced[z] = shell->input[i];
+		i++;
+		z++;
+	}
+	input_spaced[z] = '\0';
+	free(shell->input);
+	shell->input = input_spaced;
+}
+
+void	print_token_lst(t_tokens *lst)
+{
+	t_tokens	*current;
+	int			i;
+
+	current = lst;
+	i = 1;
+	while(current)
+	{
+		printf("Token %d : %s\n", i, current->content);
+		i++;
+		current = current->next;
+	}
+}
+
+void    ft_parsing_loop(t_shell *data)
+{
+	if (ft_quote_checker(data->input))  
+	{
+		data->input = expand_envvar(data, data->input);
+		ft_space_piperedir(data);
+		ft_tokenization(data);
+		print_token_lst(data->tokens);
+	}
+}
+
+void    ft_input_analizes(t_shell *data)
+{
+	ft_parsing_loop(data);
+}
---
+++ src/main.c
@@ -0,0 +1,97 @@
+#include "../inc/libs.h"
+
+t_shell *ft_start_shell(void)
+{
+	static t_shell shell;
+	return (&shell);
+}
+
+bool    ft_get_input(t_shell *data)
+{
+	char	*input;
+	char	*pwd;
+	
+	pwd = color_to_prompt(get_pathname());
+	input = readline(pwd);
+	free(pwd);
+	if (input == NULL)
+		return (false); //todo: lidar com crtl D
+	if (input[0] == 0 || only_space(input)) /* input somente espaco devolve um novo prompt */
+	{
+		free(input);
+		return (false);
+	}
+	if (input)
+	{
+		add_history(input);
+		data->input = ft_strdup(input);
+		free(input);
+	}
+	return (true);
+}
+
+int	ainput(t_shell *data)
+{
+	char	**input;
+
+	input = ft_split(data->input, ' ');
+	if (!input)
+		return (1);
+	if (ft_strcmp(input[0], "env") == 0)
+		mini_env(data->envvar);
+	else if (ft_strcmp(input[0], "export") == 0)
+		print_export(data);
+	else if (ft_strcmp(input[0], "exit") == 0)
+	{
+		free_array(input);
+		free_exit(data);
+		return (1);
+	}
+	else if (ft_strcmp(input[0], "cd") == 0)
+		cd(data, input);
+	else if (ft_strcmp(input[0], "unset") == 0)
+		unset(data, input);
+	else if (ft_strcmp(input[0], "pwd") == 0)
+		mini_pwd(data);
+	else if (ft_strcmp(input[0], "echo") == 0)
+		mini_echo(input, 1);
+	else
+		commands(data, input);
+	free_array(input);
+	return (0);
+}
+
+static void loop_those_shells(t_shell *data)
+{
+	while (1)
+	{
+		if(ft_get_input(data))
+		{
+			ft_input_analizes(data);
+			if (ainput(data))
+				break ;
+			ft_tokenclear(data->tokens);
+			free(data->input);
+		}
+	}
+}
+
+int main(int ac, char **av, char **envp)
+{
+	t_shell	*data;
+
+	if (ac != 1)
+	{
+		printf("Usage: ./minishell\n");
+		return (1);
+	}
+	(void)av;
+	data = ft_start_shell();
+	data->envvar = create_lst_envvar(envp);
+	data->envvar_export = create_lst_export(data);
+	sort_var(data->envvar_export);
+	set_shlvl(data);
+	set_questionvar(data);
+	loop_those_shells(data);
+	return (0);
+}
